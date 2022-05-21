7bf20e4d03a12326dc5d9ab122ec6df7b175ae8d
oleg-serf <olegserfcompany@gmail.com>
2022-05-21 12:10:53 -0400

    fix(ui): repo data details sync setting table

--- ui/src/views/repository-data-details/components/repo-data-type-sync-settings/settings-table/sample-data.tsx
+++ ui/src/views/repository-data-details/components/repo-data-type-sync-settings/settings-table/sample-data.tsx
@@ -2,27 +2,22 @@ import { SettingsDataType } from './@type'
 
 export const sampleDatatypesettingsData: SettingsDataType[] = [
   {
-    checked: false,
     column: 'Column 1',
     type: 'Type 1',
   },
   {
-    checked: false,
     column: 'Column 2',
     type: 'Type 2',
   },
   {
-    checked: true,
     column: 'Column 3',
     type: 'Type 3',
   },
   {
-    checked: true,
     column: 'Column 4',
     type: 'Type 4',
   },
   {
-    checked: false,
     column: 'Column 5',
     type: 'Type 5',
   },
