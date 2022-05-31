1f1eb7b12f43af537b4a79b0b4cb8bcc88ba4d89
Emily <emily.vandewalle@hotmail.com>
2022-05-31 11:27:59 +0200

    Fix typo

--- ui/src/views/repository-data/components/repository-data-logs-table/repository-data-logs-table-columns/repository-sync-status.tsx
+++ ui/src/views/repository-data/components/repository-data-logs-table/repository-data-logs-table-columns/repository-sync-status.tsx
@@ -11,7 +11,7 @@ export const RepositorySyncStatus: React.FC<RepositorySyncStatusProps> = (
   const { graphNode, disabled = false } = props
 
   return disabled ? (
-    <p className="text-sm text-samantic-mutedText">Disabld</p>
+    <p className="text-sm text-samantic-mutedText">Disabled</p>
   ) : (
     <div className='text-samantic-mutedText'>
       {graphNode}
