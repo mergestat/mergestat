e3b1e6c657b4268735e4dddd511000157b39a508
Emily <emily.vandewalle@hotmail.com>
2022-06-05 00:26:56 +0200

    Make links bolder in sync logs table

--- ui/src/views/repository-data-details/components/repo-data-type-logs/logs-table/logs-table-columns/sync-type.tsx
+++ ui/src/views/repository-data-details/components/repo-data-type-logs/logs-table/logs-table-columns/sync-type.tsx
@@ -19,7 +19,7 @@ export const SyncType: React.FC<SyncTypeProps> = (props) => {
         <div className="flex gap-2 items-center pl-6">
           <CircleErrorFilledIcon className="w-5 h-5 text-samantic-danger" />
           <Link href={`/repos/${repository}/${dataType}/${id}`}>
-            <a className="hover_text-blue-600 text-samantic-text">Failed</a>
+            <a className="font-medium hover_text-blue-600 text-samantic-text">Failed</a>
           </Link>
         </div>
       )
@@ -28,7 +28,7 @@ export const SyncType: React.FC<SyncTypeProps> = (props) => {
         <div className="flex gap-2 items-center pl-6">
           <CircleCheckFilledIcon className="w-5 h-5 text-samantic-success" />
           <Link href={`/repos/${repository}/${dataType}/${id}`.replace(/\s/g, "")}>
-            <a className="hover_text-blue-600 text-samantic-text">Succeeded</a>
+            <a className="font-medium hover_text-blue-600 text-samantic-text">Succeeded</a>
           </Link>
         </div>
       )
@@ -37,7 +37,7 @@ export const SyncType: React.FC<SyncTypeProps> = (props) => {
         <div className="flex gap-2 items-center pl-6">
           <Spinner size="sm" />
           <Link href={`/repos/${repository}/${dataType}/${id}`.replace(/\s/g, "")}>
-            <a className="hover_text-blue-600 text-samantic-text">Running</a>
+            <a className="font-medium hover_text-blue-600 text-samantic-text">Running</a>
           </Link>
         </div>
       )
