62010c1e632c87cd9bde3666e777b70759af4dcf
german-mergestat <german@mergestat.com>
2022-09-16 13:05:29 -0500

    chore(tooltip-status): Changing tooltip texts

--- ui/src/components/RepoSyncIcon.tsx
+++ ui/src/components/RepoSyncIcon.tsx
@@ -23,26 +23,32 @@ const WithTooltip: React.FC<WithTooltipPropsT> = ({ status, icon }: WithTooltipP
 }
 
 export const RepoSyncIcon = ({ type, className = '' }: RepoSyncIconPropsT) => {
-  let icon
+  let icon, text
   switch (type) {
     case SYNC_STATUS.disabled:
+      text = 'Sync is disabled'
       icon = <CircleInformationFilledIcon className={cx('t-icon text-semantic-mutedIcon', { [className]: className !== '' })} />
       break
     case SYNC_STATUS.succeeded:
+      text = 'Last sync was successful'
       icon = <CircleCheckFilledIcon className={cx('t-icon text-semantic-success', { [className]: className !== '' })} />
       break
     case SYNC_STATUS.failed:
+      text = 'Last sync had an error'
       icon = <CircleErrorFilledIcon className={cx('t-icon text-semantic-danger', { [className]: className !== '' })} />
       break
     case SYNC_STATUS.queued:
+      text = 'Sync is queued to run'
       icon = <ClockIcon className={cx('t-icon text-semantic-mutedIcon', { [className]: className !== '' })} />
       break
     case SYNC_STATUS.running:
+      text = 'Sync is currently running'
       icon = <Spinner size="sm" className={className} />
       break
     default:
+      text = 'No sync history'
       icon = <MinusIcon className={cx('t-icon text-semantic-mutedIcon', { [className]: className !== '' })} />
       break
   }
-  return <WithTooltip status={`${type.charAt(0).toUpperCase()}${type.slice(1)}`} icon={icon} />
+  return <WithTooltip status={text} icon={icon} />
 }
