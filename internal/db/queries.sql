3f9a28e106ac13ba71f9fc22733f070e8a084420
Patrick DeVivo <patrick.devivo@gmail.com>
2022-09-23 17:13:26 -0400

    fix: address bug described by #277
    See https://github.com/mergestat/fuse/issues/277 for full context

--- internal/db/queries.sql
+++ internal/db/queries.sql
@@ -111,7 +111,7 @@ WITH timed_out_sync_jobs AS (
 	UPDATE mergestat.repo_sync_queue SET status = 'DONE' WHERE status = 'RUNNING' AND (
 		(last_keep_alive < now() - '10 minutes'::interval)
 		OR
-		(last_keep_alive IS NULL AND created_at < now() - '10 minutes'::interval)) -- if worker crashed before last_keep_alive was first set
+		(last_keep_alive IS NULL AND started_at < now() - '10 minutes'::interval)) -- if worker crashed before last_keep_alive was first set
 	RETURNING *
 )
 INSERT INTO mergestat.repo_sync_logs (repo_sync_queue_id, log_type, message)
