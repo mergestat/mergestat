c6913e4afab648cb89db58fbf8986ff2d1c7db8d
Patrick DeVivo <patrick.devivo@gmail.com>
2022-09-27 09:32:59 -0400

    fix: remove `TODO` comment

--- migrations/20220927013137_git_commit_stats_file_modes.up.sql
+++ migrations/20220927013137_git_commit_stats_file_modes.up.sql
@@ -4,8 +4,8 @@ ALTER TABLE public.git_commit_stats
 ADD COLUMN old_file_mode text,
 ADD COLUMN new_file_mode text;
 
--- TODO(patrickdevivo) this will fail on existing data because new_file_mode will be NULL for all existing rows.
--- We should probably figure out how to address this before merge.
+-- Add a primary key to the table, which was omitted previously because we didn't have enough columns
+-- to guarantee uniqueness. Now that we have file modes, we can guarantee uniqueness.
 ALTER TABLE public.git_commit_stats
 ADD CONSTRAINT commit_stats_pkey PRIMARY KEY(repo_id, file_path, commit_hash, new_file_mode);
 
