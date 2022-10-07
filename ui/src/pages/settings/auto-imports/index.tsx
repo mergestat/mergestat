d92fe3f2ea8e3e77d247b13f9edb18e25c26d842
german-mergestat <german@mergestat.com>
2022-10-07 13:10:51 -0500

    feat(auto-import): Implementing auto imports unified

--- ui/src/pages/settings/auto-imports/index.tsx
+++ ui/src/pages/settings/auto-imports/index.tsx
@@ -1,17 +1,20 @@
-import { ColoredBox, Dropdown, Menu, Panel, Toolbar } from '@mergestat/blocks'
-import { CogIcon, DotsHorizontalIcon, GithubIcon, TrashIcon } from '@mergestat/icons'
-import { RepoSyncIcon } from 'src/components/RepoSyncIcon'
+import { Alert, ColoredBox, Dropdown, Menu, Panel, Toolbar } from '@mergestat/blocks'
+import { CircleCheckFilledIcon, ClockIcon, CogIcon, DotsHorizontalIcon, TrashIcon } from '@mergestat/icons'
 import type { NextPage } from 'next'
-import Link from 'next/link'
 import Head from 'next/head'
+import Link from 'next/link'
 import { Fragment } from 'react'
-import { SYNC_STATUS } from 'src/utils/constants'
+import { SYNC_REPO_METHOD } from 'src/utils/constants'
 
+import { RelativeTimeField } from 'src/components/Fields/relative-time-field'
+import Loading from 'src/components/Loading'
+import RepoImage from 'src/components/RepoImage'
+import useRepoImports from 'src/views/hooks/useRepoImports'
 import SettingsView from 'src/views/settings'
 
-const RowLink = () => {
+const RowLink = ({ importId }: { importId: string }) => {
   return (
-    <Link href='/settings/auto-imports/auto-import' passHref>
+    <Link href={`/settings/auto-imports/${importId}`} passHref>
       <a href='foo' className='t-table-row-link'>
         <span className='t-sr-only'>Go to detail</span>
       </a>
@@ -20,26 +23,7 @@ const RowLink = () => {
 }
 
 const AutoImports: NextPage = () => {
-  const autoImports = [
-    {
-      status: SYNC_STATUS.succeeded,
-      name: 'MergeStat',
-      type: 'GitHub Organization',
-      lastSync: '4 minutes ago',
-    },
-    {
-      status: SYNC_STATUS.succeeded,
-      name: 'johndoe',
-      type: 'GitHub User',
-      lastSync: '4 minutes ago',
-    },
-    {
-      status: SYNC_STATUS.succeeded,
-      name: 'codyfisher',
-      type: 'GitHub User',
-      lastSync: '4 minutes ago',
-    },
-  ]
+  const { loading, imports } = useRepoImports(true)
 
   return (
     <>
@@ -57,82 +41,95 @@ const AutoImports: NextPage = () => {
                 </Toolbar.Left>
               </Toolbar>
             </div>
-            <div className='flex-1 p-8'>
-              <Panel className='rounded-md w-full shadow-sm'>
-                <Panel.Body className='p-0'>
-                  <table className='t-table-default t-table-clickable'>
-                    <thead>
-                      <tr className='bg-white'>
-                        <th scope='col' key='name' className='whitespace-nowrap px-4'>Status</th>
-                        <th scope='col' key='name' className='whitespace-nowrap px-4'>Name</th>
-                        <th scope='col' key='name' className='whitespace-nowrap px-4'>Last Sync</th>
-                        <th scope='col' key='name' className='whitespace-nowrap px-4'></th>
-                      </tr>
-                    </thead>
+            {loading
+              ? <Loading />
+              : <div className='flex-1 p-8'>
+                <Alert type="info" className="mb-10">
+                  <strong>Repo auto imports</strong> automatically import repositories from a GitHub org or user, allowing MergeStat to pickup new repositories (and remove deleted ones) as they are added in GitHub.
+                </Alert>
 
-                    <tbody className='bg-white'>
-                      {autoImports.map((autoImport, index) => (
-                        <tr key={index}>
-                          <td className='py-4 pl-8 pr-4 w-0'>
-                            <RowLink />
-                            <RepoSyncIcon type={autoImport.status} />
-                          </td>
-                          <td className='p-4'>
-                            <RowLink />
-                            <div className='flex items-center gap-4'>
-                              <ColoredBox size='10'>
-                                <GithubIcon className='t-icon' />
-                              </ColoredBox>
-                              <div>
-                                <h4 className='font-medium'>
-                                  {autoImport.name}
-                                </h4>
-                                <p className='text-sm text-semantic-mutedText'>
-                                  {autoImport.type}
-                                </p>
-                              </div>
-                            </div>
-                          </td>
-                          <td className='text-gray-500 p-4'>
-                            <RowLink />
-                            {autoImport.lastSync}
-                          </td>
-                          <td className='text-gray-500 py-4 pl-4 pr-8'>
-                            <RowLink />
-                            <div className='t-button-toolbar'>
-                              <Dropdown
-                                alignEnd
-                                trigger={
-                                  <DotsHorizontalIcon className='t-icon cursor-pointer' />
+                <Panel className='rounded-md w-full shadow-sm'>
+                  <Panel.Body className='p-0'>
+                    {imports.length < 1
+                      ? <div className='flex justify-center items-center bg-white py-5'>
+                        No data available!
+                      </div>
+                      : <table className='t-table-default t-table-clickable'>
+                        <thead>
+                          <tr className='bg-white'>
+                            <th scope='col' key='name' className='whitespace-nowrap px-4'>Status</th>
+                            <th scope='col' key='name' className='whitespace-nowrap px-4'>Name</th>
+                            <th scope='col' key='name' className='whitespace-nowrap px-4'>Last Sync</th>
+                            <th scope='col' key='name' className='whitespace-nowrap px-4'></th>
+                          </tr>
+                        </thead>
+
+                        <tbody className='bg-white'>
+                          {imports.map((imp, index) => (
+                            <tr key={index}>
+                              <td className='py-4 pl-8 pr-4 w-0'>
+                                <RowLink importId={imp.id} />
+                                {imp.importDone
+                                  ? <CircleCheckFilledIcon className="t-icon text-semantic-success" />
+                                  : <ClockIcon className='t-icon text-semantic-mutedIcon' />
                                 }
-                                overlay={() => (
-                                  <Menu className='whitespace-nowrap'>
-                                    <Menu.Item
-                                      text='Auto Import settings'
-                                      withIcon
-                                      href='/settings/auto-imports/auto-import'
-                                      icon={<CogIcon className='t-icon mr-2' />}
-                                    />
-                                    <Menu.Divider />
-                                    <Menu.Item
-                                      text='Remove Auto Import'
-                                      withIcon
-                                      icon={
-                                        <TrashIcon className='t-icon mr-2' />
-                                      }
-                                    />
-                                  </Menu>
-                                )}
-                              />
-                            </div>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </Panel.Body>
-              </Panel>
-            </div>
+                              </td>
+                              <td className='p-4'>
+                                <RowLink importId={imp.id} />
+                                <div className='flex items-center gap-4'>
+                                  <ColoredBox size='10'>
+                                    <RepoImage repoType='github' orgName={imp.source} size="10" />
+                                  </ColoredBox>
+                                  <div>
+                                    <h4 className='font-medium'>
+                                      {imp.source}
+                                    </h4>
+                                    <p className='text-sm text-semantic-mutedText'>
+                                      {imp.type === SYNC_REPO_METHOD.GH_USER ? 'GitHub User' : 'GitHub Organization'}
+                                    </p>
+                                  </div>
+                                </div>
+                              </td>
+                              <td className='text-gray-500 p-4'>
+                                <RowLink importId={imp.id} />
+                                <RelativeTimeField date={imp.lastSync} />
+                              </td>
+                              <td className='text-gray-500 py-4 pl-4 pr-8'>
+                                <RowLink importId={imp.id} />
+                                <div className='t-button-toolbar'>
+                                  <Dropdown
+                                    alignEnd
+                                    trigger={
+                                      <DotsHorizontalIcon className='t-icon cursor-pointer' />
+                                    }
+                                    overlay={() => (
+                                      <Menu className='whitespace-nowrap'>
+                                        <Menu.Item
+                                          text='Auto Import settings'
+                                          withIcon
+                                          href='/settings/auto-imports/auto-import'
+                                          icon={<CogIcon className='t-icon mr-2' />}
+                                        />
+                                        <Menu.Divider />
+                                        <Menu.Item
+                                          text='Remove Auto Import'
+                                          withIcon
+                                          icon={
+                                            <TrashIcon className='t-icon mr-2' />
+                                          }
+                                        />
+                                      </Menu>
+                                    )}
+                                  />
+                                </div>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>}
+                  </Panel.Body>
+                </Panel>
+              </div>}
           </div>
         </SettingsView>
       </Fragment>
