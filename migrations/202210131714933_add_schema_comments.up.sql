8994c931dc0c68385470bb1d9a536d94429bd65f
Josue Lopez <josue@mergestat.com>
2022-10-12 18:42:48 -0500

    Update migrations/202210131714933_add_schema_comments.up.sql
    Co-authored-by: Patrick DeVivo <patrick.devivo@gmail.com>

--- migrations/202210131714933_add_schema_comments.up.sql
+++ migrations/202210131714933_add_schema_comments.up.sql
@@ -197,7 +197,7 @@ COMMENT ON COLUMN public.github_repo_info.latest_release_name IS 'the name of th
 COMMENT ON COLUMN public.github_repo_info.latest_release_published_at IS 'timestamp of the publishing of the latest release in the repo';
 COMMENT ON COLUMN public.github_repo_info.license_key IS 'the license key for the repo';
 COMMENT ON COLUMN public.github_repo_info.license_name IS 'the license name for the repo';
-COMMENT ON COLUMN public.github_repo_info.license_nickname IS 'the licence nickname for the repo';
+COMMENT ON COLUMN public.github_repo_info.license_nickname IS 'the license nickname for the repo';
 COMMENT ON COLUMN public.github_repo_info.open_graph_image_url IS 'the URL for the image used to represent this repository in Open Graph data';
 COMMENT ON COLUMN public.github_repo_info.primary_language IS 'the primary language for the repo';
 COMMENT ON COLUMN public.github_repo_info.pushed_at IS 'timestamp of the latest push to the repo';
