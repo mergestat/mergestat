f2f0f14bff4577b852d9d5bd4c82425c32c66270
Ramiro <ramirojesuscastillo95@gmail.com>
2022-10-17 10:01:21 -0500

    fix(mergestat): fixing vscode bug

--- migrations/20221010082656_fix_pr_reviews_pk.up 2.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE public.github_pull_request_reviews
-DROP CONSTRAINT IF EXISTS github_pull_request_reviews_pkey;
-
-ALTER TABLE public.github_pull_request_reviews
-ADD CONSTRAINT github_pull_request_reviews_pkey PRIMARY KEY(repo_id, id);
-
-COMMIT;
