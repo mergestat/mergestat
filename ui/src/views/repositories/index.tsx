87cbeb31c7edc61968cff1007b7a085b4d727b98
Emily <emily.vandewalle@hotmail.com>
2022-10-25 14:50:24 +0200

    feat: update metrics on repo overview

--- ui/src/views/repositories/index.tsx
+++ ui/src/views/repositories/index.tsx
@@ -3,7 +3,7 @@ import { EmptyRepositoryTable, FilterHeader, PageHeader, RepositoriesTable } fro
 import { AddRepositoryModal } from './modals/add-repository-modal'
 
 import { Alert, ColoredBox, Spinner, Stat } from '@mergestat/blocks'
-import { ClockIcon, TableIcon, RepositoryIcon } from '@mergestat/icons'
+import { CircleErrorIcon, CircleErrorFilledIcon, TableIcon, RepositoryIcon } from '@mergestat/icons'
 import Loading from 'src/components/Loading'
 import useRepos from 'src/views/hooks/useRepos'
 import { EmptyRepository } from './components/empty-repository'
@@ -13,18 +13,13 @@ const RepositoriesView: React.FC = () => {
   const [{ showAddRepositoryModal, showRemoveRepositoryModal }] = useRepositoriesContext()
   const { showTable, loading, data, showBanner } = useRepos(true)
 
-  const AvgSync = () => {
-    if (data && data.repos && data.allRepos) {
-      return Math.round(data.repos.nodes.reduce((total, next) => total + next.repoSyncs.totalCount, 0) / data.allRepos.totalCount)
-    }
-  }
+  /* TODO: replace with real number */
+  const syncErrors = 0
+  const repoSyncs = 8
 
   return (
     <main className='w-full flex flex-col h-full bg-gray-50 overflow-hidden'>
-      <div className='bg-white border-b border-gray-200'>
-        <PageHeader />
-        {showTable && <FilterHeader />}
-      </div>
+      <PageHeader />
       <div className='flex-1 items-center p-8 overflow-auto'>
         {showBanner &&
           <Alert
@@ -43,45 +38,57 @@ const RepositoriesView: React.FC = () => {
         }
 
         {/* Metrics */}
-        <div className='md_grid md_grid-cols-3 gap-6 space-y-4 md_space-y-0 mb-6'>
-          <Stat className='shadow-sm w-full'>
-            <Stat.Left>
-                <Stat.Label>Number of repos</Stat.Label>
-                <Stat.Number>{data?.allRepos?.totalCount}</Stat.Number>
-            </Stat.Left>
-            <Stat.Right>
-              <ColoredBox size='12'><RepositoryIcon className='t-icon t-icon-default' /></ColoredBox>
-            </Stat.Right>
-          </Stat>
-          <Stat className='shadow-sm w-full'>
-            <Stat.Left>
-                <Stat.Label>Avg. number of syncs</Stat.Label>
-                <Stat.Number>{AvgSync()}</Stat.Number>
+        {showTable &&
+          <div className='md_grid md_grid-cols-3 gap-6 space-y-4 md_space-y-0 mb-6'>
+            <Stat className='shadow-sm w-full'>
+              <Stat.Left>
+                  <Stat.Label>Number of repos</Stat.Label>
+                  <Stat.Number>{data?.allRepos?.totalCount}</Stat.Number>
+              </Stat.Left>
+              <Stat.Right>
+                <ColoredBox size='12'><RepositoryIcon className='t-icon t-icon-default' /></ColoredBox>
+              </Stat.Right>
+            </Stat>
+            <Stat className='shadow-sm w-full'>
+              <Stat.Left>
+                  <Stat.Label>Total number of repo syncs</Stat.Label>
+                  <Stat.Number>{repoSyncs}</Stat.Number>
+              </Stat.Left>
+              <Stat.Right>
+                <ColoredBox size='12'><TableIcon className='t-icon t-icon-default' /></ColoredBox>
+              </Stat.Right>
+            </Stat>
+            <Stat className='shadow-sm w-full'>
+              <Stat.Left>
+                  <Stat.Label>Total repo sync errors</Stat.Label>
+                  {/* TODO: convert static number to calculated */}
+                  <Stat.Number>
+                    <div className='flex items-center space-x-1.5'>
+                    {syncErrors > 0 && <CircleErrorFilledIcon className='t-icon t-icon-danger' />}
+                    <span>{syncErrors}</span>
+                    </div>
+                  </Stat.Number>
+              </Stat.Left>
+              <Stat.Right>
+                <ColoredBox size='12'><CircleErrorIcon className='t-icon t-icon-default' /></ColoredBox>
+              </Stat.Right>
+            </Stat>
+          </div>
+        }
 
-            </Stat.Left>
-            <Stat.Right>
-              <ColoredBox size='12'><TableIcon className='t-icon t-icon-default' /></ColoredBox>
-            </Stat.Right>
-          </Stat>
-          <Stat className='shadow-sm w-full'>
-            <Stat.Left>
-                <Stat.Label>Avg. sync run time</Stat.Label>
-                {/* TODO: convert static number to calculated */}
-                <Stat.Number>1m</Stat.Number>
-            </Stat.Left>
-            <Stat.Right>
-              <ColoredBox size='12'><ClockIcon className='t-icon t-icon-default' /></ColoredBox>
-            </Stat.Right>
-          </Stat>
-        </div>
+        {showTable && <div className='mb-8'><FilterHeader /></div>}
+
         {loading
           ? <Loading />
-          : data?.allRepos?.totalCount && data?.allRepos?.totalCount > 0
-            ? <RepositoriesTable data={data} />
+          : showTable
+            ? <>
+                {/* Repo table */}
+                <RepositoriesTable data={data} />
+            </>
             : data?.serviceAuthCredentials?.totalCount && data?.serviceAuthCredentials?.totalCount > 0
               ? <EmptyRepository />
               : <EmptyRepositoryTable />
-        }
+            }
       </div>
 
       {showRemoveRepositoryModal && <RemoveRepositoryModal />}
