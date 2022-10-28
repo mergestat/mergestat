b30872131af637f8b35c60cd6962137c67c36c2c
german-mergestat <german@mergestat.com>
2022-10-28 15:38:43 -0500

    chore(repos): Redirecting to repos when given id does not exist

--- ui/src/views/repository-data/index.tsx
+++ ui/src/views/repository-data/index.tsx
@@ -1,10 +1,9 @@
 import { Tabs } from '@mergestat/blocks'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import { RepoSyncData } from 'src/@types'
 import { useRepositoriesContext } from 'src/state/contexts'
 import { TEST_IDS } from 'src/utils/constants'
-import { EmptyRepositoryTable } from '../repositories/components'
-import { EmptyRepository } from '../repositories/components/empty-repository'
 import { AddRepositoryModal } from '../repositories/modals/add-repository-modal'
 import { RemoveRepositoryModal } from '../repositories/modals/remove-repository-modal'
 import { PageHeader, RepoSettings, SycnTypesTable } from './components'
@@ -16,36 +15,36 @@ type RepoDataViewProps = {
 
 const RepoDataView = ({ data }: RepoDataViewProps) => {
   const [{ showAddRepositoryModal, showRemoveRepositoryModal }] = useRepositoriesContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    !data?.id && router.push('/repos')
+  }, [data, router])
 
   return (
     <main className="w-full flex flex-col h-full overflow-hidden bg-gray-50">
-      {data?.id
-        ? <>
-          <PageHeader name={data?.name || ''} type={data?.type || 'other'} />
-          <Tabs.Group>
-            <Tabs.List className="bg-white w-full justify-between px-8 items-center border-b border-gray-200">
-              <Tabs.Item className="ring-transparent focus_ring-transparent">
-                Sync Types
-              </Tabs.Item>
-              <Tabs.Item data-testid={TEST_IDS.removeRepoSettingsTab}>Repo Settings</Tabs.Item>
-            </Tabs.List>
-            <Tabs.Panels className="p-8 flex-1 overflow-auto">
-              <Tabs.Panel>
-                <SycnTypesTable repoId={data?.id || ''} data={data?.syncs || []} />
-              </Tabs.Panel>
-              <Tabs.Panel>
-                <RepoSettings
-                  id={data.id}
-                  name={data.name}
-                  tags={data.tags}
-                  autoImported={!!data.autoImportFrom}
-                />
-              </Tabs.Panel>
-            </Tabs.Panels>
-          </Tabs.Group>
-        </>
-        : data?.gitHubPat ? <EmptyRepository /> : <EmptyRepositoryTable />
-      }
+      <PageHeader name={data?.name || ''} type={data?.type || 'other'} />
+      <Tabs.Group>
+        <Tabs.List className="bg-white w-full justify-between px-8 items-center border-b border-gray-200">
+          <Tabs.Item className="ring-transparent focus_ring-transparent">
+            Sync Types
+          </Tabs.Item>
+          <Tabs.Item data-testid={TEST_IDS.removeRepoSettingsTab}>Repo Settings</Tabs.Item>
+        </Tabs.List>
+        <Tabs.Panels className="p-8 flex-1 overflow-auto">
+          <Tabs.Panel>
+            <SycnTypesTable repoId={data?.id || ''} data={data?.syncs || []} />
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <RepoSettings
+              id={data?.id || ''}
+              name={data?.name || ''}
+              tags={data?.tags || []}
+              autoImported={!!data?.autoImportFrom}
+            />
+          </Tabs.Panel>
+        </Tabs.Panels>
+      </Tabs.Group>
       {showAddRepositoryModal && <AddRepositoryModal />}
       {showRemoveRepositoryModal && <RemoveRepositoryModal />}
     </main>
