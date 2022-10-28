fd3e897c832dc5fa3d3dce3c7e0fc78b5a747425
german-mergestat <german@mergestat.com>
2022-10-28 13:32:03 -0500

    chore(query-editor): Changing to MinusIcon

--- ui/src/views/sql-query-editor/components/state-empty.tsx
+++ ui/src/views/sql-query-editor/components/state-empty.tsx
@@ -1,5 +1,5 @@
 import { Avatar } from '@mergestat/blocks'
-import { CircleInformationIcon } from '@mergestat/icons'
+import { CircleInformationIcon, MinusIcon } from '@mergestat/icons'
 
 type QueryEditorEmptyProps = {
   executed: boolean
@@ -9,7 +9,7 @@ type QueryEditorEmptyProps = {
 const QueryEditorEmpty: React.FC<QueryEditorEmptyProps> = ({ executed }: QueryEditorEmptyProps) => {
   return (
     <div className='flex-1 h-1/2 flex flex-col items-center justify-center p-8'>
-      <Avatar icon={<CircleInformationIcon className='t-icon' />} />
+      <Avatar icon={executed ? <MinusIcon className="t-icon" /> : <CircleInformationIcon className='t-icon' />} />
       <p className='t-text-muted mt-6'>
         {executed ? 'Empty result set (no rows returned).' : 'Execute query to show results.'}
       </p>
