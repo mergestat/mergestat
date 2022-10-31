3195c04db8fa36677bfd58b83c26dd06f4665d1f
Ramiro <ramirojesuscastillo95@gmail.com>
2022-10-31 14:37:39 -0500

    fix(helper): removing dead code

--- internal/helper/helper_test.go
+++ internal/helper/helper_test.go
@@ -2,8 +2,6 @@ package helper
 
 import (
 	"testing"
-
-	"github.com/rs/zerolog"
 )
 
 func TestGetRepoOwnerAndRepoName(t *testing.T) {
@@ -45,38 +43,6 @@ func TestGetRepoOwnerAndRepoName(t *testing.T) {
 			if gotRepo != test.wantRepo {
 				t.Errorf("GetRepoOwnerAndRepoName gotRepo = %v, want %v", gotRepo, test.wantRepo)
 			}
-		})
-	}
-}
-
-func TestCreateTempDir(t *testing.T) {
-	type args struct {
-		l       *zerolog.Logger
-		envPath string
-		pattern string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		want1   func()
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			/*got, got1, err := CreateTempDir(tt.args.l, tt.args.envPath, tt.args.pattern)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CreateTempDir() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("CreateTempDir() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("CreateTempDir() got1 = %v, want %v", got1, tt.want1)
-			}*/
 		})
 	}
 }
