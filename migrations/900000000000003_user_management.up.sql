1b1ae1892d55ac3bda75c98f54ee9686a9ac1928
Patrick DeVivo <patrick.devivo@gmail.com>
2022-11-07 23:21:01 -0500

    feat: add `set_user_role` func
    to separate out `authn` and `authz`

--- migrations/900000000000003_user_management.up.sql
+++ migrations/900000000000003_user_management.up.sql
@@ -15,21 +15,43 @@ DECLARE
 BEGIN
     -- Create the user with the given password
     EXECUTE FORMAT('CREATE USER %I WITH PASSWORD %L', username, password);
+    RETURN 1;
+END;
+$BODY$
+LANGUAGE plpgsql STRICT VOLATILE SECURITY DEFINER;
 
-    -- Grant the user access to the public schema
-    EXECUTE FORMAT('GRANT USAGE ON SCHEMA public TO %I', username);
-    EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO %I', username);
-    EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO %I', username);
-    EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO %I', username); -- Ensure all tables and sequences created in the future have all privileges
-    EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO %I', username);
+-- Function to set the role of a user
+DROP FUNCTION IF EXISTS mergestat.set_user_role(NAME, TEXT);
 
-    -- Grant the user access to the mergestat schema
-    EXECUTE FORMAT('GRANT USAGE ON SCHEMA mergestat TO %I', username);
-    EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA mergestat TO %I', username);
-    EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA mergestat TO %I', username);
-    EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA mergestat GRANT ALL PRIVILEGES ON TABLES TO %I', username);
-    EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA mergestat GRANT ALL PRIVILEGES ON SEQUENCES TO %I', username);
+CREATE OR REPLACE FUNCTION mergestat.set_user_role(username NAME, role TEXT)
+RETURNS SMALLINT AS
+$BODY$
+DECLARE
+BEGIN
+    -- first revoke all privileges from the user
+    EXECUTE FORMAT('DROP OWNED BY %I', username);
+    CASE
+        WHEN role = 'ADMIN' THEN
+            -- Grant the user access to the public schema
+            EXECUTE FORMAT('GRANT USAGE ON SCHEMA public TO %I', username);
+            EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO %I', username);
+            EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO %I', username);
+            EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO %I', username); -- Ensure all tables and sequences created in the future have all privileges
+            EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON SEQUENCES TO %I', username);
 
+            -- Grant the user access to the mergestat schema
+            EXECUTE FORMAT('GRANT USAGE ON SCHEMA mergestat TO %I', username);
+            EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA mergestat TO %I', username);
+            EXECUTE FORMAT('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA mergestat TO %I', username);
+            EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA mergestat GRANT ALL PRIVILEGES ON TABLES TO %I', username);
+            EXECUTE FORMAT('ALTER DEFAULT PRIVILEGES IN SCHEMA mergestat GRANT ALL PRIVILEGES ON SEQUENCES TO %I', username);
+        WHEN role = 'USER' THEN
+            -- TODO(patrickdevivo) implement authz for USER role
+        WHEN role = 'READ_ONLY' THEN
+            -- TODO(patrickdevivo) implement authz for READ_ONLY role
+        ELSE
+            RAISE EXCEPTION 'Invalid role %', role;
+    END;
     RETURN 1;
 END;
 $BODY$
@@ -43,7 +65,7 @@ RETURNS SMALLINT AS
 $BODY$
 DECLARE
 BEGIN
-    EXECUTE FORMAT('DROP OWNED BY %I', username); -- First drop all objects owned by the user
+    EXECUTE FORMAT('DROP OWNED BY %I', username); -- first drop all objects owned by the user
     EXECUTE FORMAT('DROP USER IF EXISTS %I', username);
     RETURN 1;
 END;
