7fc7ef719a76afbf50c428f1beb31850e3a88da0
german-mergestat <german@mergestat.com>
2022-11-08 16:28:47 -0500

    chore(linting): Fixing linting errors

--- ui/src/views/settings/modals/edit-user-modal/index.tsx
+++ ui/src/views/settings/modals/edit-user-modal/index.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Button, Input, Label, Modal, Panel, Toolbar } from '@mergestat/blocks'
 import { UserIcon, XIcon } from '@mergestat/icons'
-import React, { useCallback, useState } from 'react'
 import cx from 'classnames'
+import React, { ChangeEvent, useCallback, useState } from 'react'
 import { useUserSettingsSetState } from 'src/state/contexts/user-settings.context'
 
 const roles = [
@@ -28,7 +28,7 @@ export const EditUserModal: React.FC = () => {
 
   const [Role, setRole] = useState('Admin')
 
-  function onChangeValue(event: any) {
+  function onChangeValue(event: ChangeEvent<HTMLInputElement>) {
     setRole(event.target.value)
   }
 
@@ -56,13 +56,13 @@ export const EditUserModal: React.FC = () => {
         <div className='p-6'>
           <Panel className='mb-6'>
             <Panel.Body>
-                <div className='flex items-center space-x-4'>
-                    <Avatar size='lg' icon={<UserIcon className='t-icon' />} />
-                    <div>
-                        <h4 className='font-medium mb-0.5'>johndoe</h4>
-                        <p className='t-text-muted text-sm'>Created 4 days ago</p>
-                    </div>
+              <div className='flex items-center space-x-4'>
+                <Avatar size='lg' icon={<UserIcon className='t-icon' />} />
+                <div>
+                  <h4 className='font-medium mb-0.5'>johndoe</h4>
+                  <p className='t-text-muted text-sm'>Created 4 days ago</p>
                 </div>
+              </div>
             </Panel.Body>
           </Panel>
           <form className='space-y-6'>
@@ -76,23 +76,23 @@ export const EditUserModal: React.FC = () => {
             </div>
             <div>
               <Label>Role</Label>
-                <div className='space-y-3'>
-                  {roles.map((role, index) => (
-                    <div key={index} onChange={onChangeValue}>
-                      <label className='t-radio space-x-4'>
-                        <div className={cx('t-radio-card w-full', { 't-radio-card-selected': Role === role.name })}>
-                          <div className='self-start'>
-                            <input type='radio' name='role' value={role.name} checked={ role ? Role === role.name : undefined } />
-                          </div>
-                          <div>
-                            <h4 className='font-medium mb-0.5'>{role.name}</h4>
-                            <p className='font-normal text-sm t-text-muted'>{role.desc}</p>
-                          </div>
+              <div className='space-y-3'>
+                {roles.map((role, index) => (
+                  <div key={index} onChange={onChangeValue}>
+                    <label htmlFor={`radio-role-${index}`} className='t-radio space-x-4'>
+                      <div className={cx('t-radio-card w-full', { 't-radio-card-selected': Role === role.name })}>
+                        <div className='self-start'>
+                          <input id={`radio-role-${index}`} type='radio' name='role' value={role.name} checked={role ? Role === role.name : undefined} />
                         </div>
-                      </label>
-                    </div>
-                  ))}
-                </div>
+                        <div>
+                          <h4 className='font-medium mb-0.5'>{role.name}</h4>
+                          <p className='font-normal text-sm t-text-muted'>{role.desc}</p>
+                        </div>
+                      </div>
+                    </label>
+                  </div>
+                ))}
+              </div>
             </div>
           </form>
         </div>
