a273130da0033919d4945e986d69791d2f344b40
Emily <emily.vandewalle@hotmail.com>
2022-12-03 13:50:06 +0100

    feat: start sidebar layout

--- ui/src/views/settings/index.tsx
+++ ui/src/views/settings/index.tsx
@@ -1,7 +1,4 @@
 
-import { Toolbar, VerticalNav } from '@mergestat/blocks'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 interface Props {
@@ -9,42 +6,9 @@ interface Props {
 }
 
 const SettingsView = ({ children }: Props) => {
-  const { pathname } = useRouter() || { pathname: { text: '' } }
-
-  const active = (pathMenu: string) => pathname === pathMenu
-
   return (
     <main className='w-full flex flex-col h-full bg-gray-50 overflow-hidden'>
-      <div className='flex h-full'>
-        {/* Sidebar content */}
-        <div className='flex flex-col w-56 md_w-72 h-full border-r'>
-          <div className='bg-white h-16 w-full border-b px-8'>
-            <Toolbar className='h-full'>
-              <Toolbar.Left>
-                <h2 className='t-h2 mb-0'>Settings</h2>
-              </Toolbar.Left>
-            </Toolbar>
-          </div>
-          <div className='flex-1 overflow-auto p-8'>
-            <VerticalNav>
-              {/* <VerticalNav.Item text='General' disabled={true} /> */}
-              <Link href='/settings'>
-                <VerticalNav.Item href='/settings' text='GitHub Authentication' active={active('/settings')} />
-              </Link>
-              <Link href='/settings/repo-auto-imports'>
-                <VerticalNav.Item href='/settings/repo-auto-imports' text='Repo Auto Imports' active={active('/settings/repo-auto-imports') || active('/settings/repo-auto-imports/[importId]')} />
-              </Link>
-              <Link href='/settings/user-management'>
-                <VerticalNav.Item href='/settings/user-management' text='User Management' active={active('/settings/user-management')} />
-              </Link>
-              <Link href='/settings/user-settings'>
-                <VerticalNav.Item href='/settings/user-settings' text='User Settings' active={active('/settings/user-settings')} />
-              </Link>
-            </VerticalNav>
-          </div>
-        </div>
-        {children}
-      </div>
+      {children}
     </main>
   )
 }
