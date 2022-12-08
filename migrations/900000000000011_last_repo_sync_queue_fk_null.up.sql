93c4eb49ce21bc88808c874c6c1634b147d82850
Josue Lopez <amenowanna@hotmail.com>
2022-12-07 22:57:30 -0600

    refactor: added logic to reset dirty migrtions

--- migrations/900000000000011_last_repo_sync_queue_fk_null.up.sql
+++ migrations/900000000000011_last_repo_sync_queue_fk_null.up.sql
@@ -13,4 +13,19 @@ ADD CONSTRAINT last_completed_repo_sync_queue_id_fk
 FOREIGN KEY (last_completed_repo_sync_queue_id)
 REFERENCES mergestat.repo_sync_queue(id) ON DELETE SET NULL;
 
+-- update trigger to reset migration to not dirty to allow re-run of migration without intervention
+CREATE OR REPLACE FUNCTION track_applied_migration()
+RETURNS trigger AS $$
+DECLARE _current_version BIGINT;
+BEGIN
+    SELECT COALESCE(MAX(version),0) FROM public.schema_migrations_history INTO _current_version;
+    IF new.dirty = false AND new.version > _current_version THEN
+        INSERT INTO public.schema_migrations_history(version) VALUES (new.version);
+    ELSE
+        UPDATE public.schema_migrations SET version = (SELECT MAX(version) FROM public.schema_migrations_history), dirty = false;
+    END IF;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
 COMMIT;
