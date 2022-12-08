d08d04e2ae1540e40bd3c91252d2ccd4a75cd740
Josue Lopez <amenowanna@hotmail.com>
2022-12-07 22:02:50 -0600

    fix: migration 11

--- migrations/900000000000011_last_repo_sync_queue_fk_null.up.sql
+++ migrations/900000000000011_last_repo_sync_queue_fk_null.up.sql
@@ -1,8 +1,16 @@
 BEGIN;
 
-ALTER TABLE "mergestat"."repo_syncs"
-ADD CONSTRAINT "last_completed_repo_sync_queue_id_fk"
-FOREIGN KEY ("last_completed_repo_sync_queue_id")
-REFERENCES "mergestat"."repo_sync_queue"("id");
+--Cleanup any orphaned id's
+UPDATE mergestat.repo_syncs
+SET last_completed_repo_sync_queue_id = NULL
+WHERE last_completed_repo_sync_queue_id NOT IN (SELECT id FROM mergestat.repo_sync_queue);
+
+ALTER TABLE mergestat.repo_syncs
+DROP CONSTRAINT IF EXISTS last_completed_repo_sync_queue_id_fk;
+
+ALTER TABLE mergestat.repo_syncs
+ADD CONSTRAINT last_completed_repo_sync_queue_id_fk
+FOREIGN KEY (last_completed_repo_sync_queue_id)
+REFERENCES mergestat.repo_sync_queue(id) ON DELETE SET NULL;
 
 COMMIT;
