60b64da1f0e31bad8b7bf209e9a511ad0fc9c843
Ramiro <ramirojesuscastillo95@gmail.com>
2022-12-09 12:19:14 -0500

    fix(migrations):Removing extra migrations

--- migrations/900000000000011_jobs_null.up.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-ALTER TABLE public.github_actions_workflow_runs
-ALTER COLUMN ID DROP NOT NULL;
-
-ALTER TABLE public.github_actions_workflow_run_jobs
-ALTER COLUMN ID DROP NOT NULL;
-
-COMMIT;
