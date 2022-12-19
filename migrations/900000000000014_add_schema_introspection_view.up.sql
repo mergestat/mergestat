65d27dbb43ad71a403ee2cd14e130f5f1ff9520f
Patrick DeVivo <patrick.devivo@gmail.com>
2022-12-19 09:55:20 -0500

    chore: add `column_description` column

--- migrations/900000000000014_add_schema_introspection_view.up.sql
+++ migrations/900000000000014_add_schema_introspection_view.up.sql
@@ -12,7 +12,8 @@ CREATE OR REPLACE VIEW mergestat.schema_introspection AS (
         information_schema.columns.ordinal_position,
         information_schema.columns.is_nullable,
         information_schema.columns.data_type,
-        information_schema.columns.udt_name
+        information_schema.columns.udt_name,
+        pg_catalog.col_description(format('%s.%s', information_schema.columns.table_schema, information_schema.columns.table_name)::regclass::oid, information_schema.columns.ordinal_position) as column_description
     FROM information_schema.tables
     INNER JOIN information_schema.columns ON (information_schema.tables.table_name = information_schema.columns.table_name AND information_schema.tables.table_schema = information_schema.columns.table_schema)
     WHERE information_schema.tables.table_schema IN ('public', 'mergestat')
