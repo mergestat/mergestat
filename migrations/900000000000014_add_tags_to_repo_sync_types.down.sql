ee56023302c8ca32f8573402d8b3d011c5953adc
Patrick DeVivo <patrick.devivo@gmail.com>
2022-12-19 10:58:00 -0500

    fix: migration conflict and updated table name

--- migrations/900000000000014_add_tags_to_repo_sync_types.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS mergestat.repo_sync_type_tagged;
-DROP TABLE IF EXISTS mergestat.repo_sync_type_labels;
---
+++ migrations/900000000000015_add_tags_to_repo_sync_types.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS mergestat.repo_sync_type_label_associations;
+DROP TABLE IF EXISTS mergestat.repo_sync_type_labels;
