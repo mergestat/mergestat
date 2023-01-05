3420917753af83d3ad3ef086c9b0666739e04068
Patrick DeVivo <patrick.devivo@gmail.com>
2023-01-04 20:09:57 -0500

    fix: remove extraneous button for clarity

--- ui/src/views/repositories/components/empty-repository.tsx
+++ ui/src/views/repositories/components/empty-repository.tsx
@@ -1,24 +1,14 @@
-import { Avatar, Button } from '@mergestat/blocks'
-import { PlusIcon, RepositoryIcon } from '@mergestat/icons'
+import { Avatar } from '@mergestat/blocks'
+import { RepositoryIcon } from '@mergestat/icons'
 import React from 'react'
-import { useRepositoriesSetState } from 'src/state/contexts/repositories.context'
 import { TEST_IDS } from 'src/utils/constants'
 
 export const EmptyRepository: React.FC = () => {
-  const { setShowAddRepositoryModal } = useRepositoriesSetState()
-
   return (
     <div data-testid={TEST_IDS.emptyRepository} className="flex items-center justify-center h-full w-full">
       <div className="flex flex-col items-center">
         <Avatar icon={<RepositoryIcon className='t-icon' />} size='lg' />
         <p className="t-text-muted py-5">No repositories added yet</p>
-        <Button
-          startIcon={<PlusIcon className="t-icon" />}
-          onClick={() => setShowAddRepositoryModal(true)}
-          skin="secondary"
-        >
-          Add Repository
-        </Button>
       </div>
     </div>
   )
