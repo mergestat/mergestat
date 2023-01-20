a723da8df85833189e358297df4782a1c8e400c1
Josue Lopez <amenowanna@hotmail.com>
2023-01-20 15:50:32 -0600

    fix(examples): updated initiate syncing queries

--- examples/management/sql/initiate-syncing.sql
+++ examples/management/sql/initiate-syncing.sql
@@ -7,19 +7,24 @@ VALUES
 ON CONFLICT DO NOTHING;
 
 --add all available syncs to all repos
-INSERT INTO mergestat.repo_syncs (repo_id, sync_type)
+INSERT INTO mergestat.repo_syncs (repo_id, sync_type, priority, schedule_enabled)
 SELECT
     public.repos.id,
-    mergestat.repo_sync_types.type
+    mergestat.repo_sync_types.type,
+    mergestat.repo_sync_types.priority,
+    TRUE AS schedule_enabled
 FROM public.repos CROSS JOIN mergestat.repo_sync_types
 ON CONFLICT DO NOTHING;
 
 --add all syncs to the queue
-INSERT INTO mergestat.repo_sync_queue (repo_sync_id, status)
+INSERT INTO mergestat.repo_sync_queue (repo_sync_id, status, priority, type_group)
 SELECT
     id,
-    'QUEUED' AS status
+    'QUEUED' AS status,
+    mergestat.repo_syncs.priority,
+    mergestat.repo_sync_types.type_group
 FROM mergestat.repo_syncs
+INNER JOIN mergestat.repo_sync_types ON mergestat.repo_syncs.sync_type = mergestat.repo_sync_types.type
 ON CONFLICT DO NOTHING;
 
 --check sync queue
