018ffb079506334a00680a096559373357b56fd3
Ramiro <ramirojesuscastillo95@gmail.com>
2023-02-03 12:05:38 -0500

    chore(migrations): Small fixes

--- migrations/900000000000025_grype_synced_at.up.sql
+++ migrations/900000000000025_grype_synced_at.up.sql
@@ -4,8 +4,8 @@ ALTER TABLE public.grype_repo_scans
 ADD COLUMN _mergestat_synced_at timestamp with time zone DEFAULT now() NOT NULL;
 
 COMMENT ON TABLE public.grype_repo_scans IS 'Table for Grype repo scan results';
-COMMENT ON COLUMN grype_repo_scans.repo_id IS 'ID of repository which grype was executed';
-COMMENT ON COLUMN grype_repo_scans.results IS 'Json result of Grype repo scanner';
-COMMENT ON COLUMN grype_repo_scans._mergestat_synced_at IS 'Time when syncer completed its execution';
+COMMENT ON COLUMN grype_repo_scans.repo_id IS 'foreign key for public.repos.id';
+COMMENT ON COLUMN grype_repo_scans.results IS 'JSON results of Grype repo scanner';
+COMMENT ON COLUMN grype_repo_scans._mergestat_synced_at IS 'timestamp when record was synced into the Mergestat database';
 
 COMMIT;
