d48febbddedcafa58b65c06780a192114ed3ad12
Ramiro Castillo <43893061+ramiroJCB@users.noreply.github.com>
2023-02-03 12:18:41 -0500

    Update migrations/900000000000025_grype_synced_at.up.sql
    Co-authored-by: Patrick DeVivo <patrick.devivo@gmail.com>

--- migrations/900000000000025_grype_synced_at.up.sql
+++ migrations/900000000000025_grype_synced_at.up.sql
@@ -6,6 +6,6 @@ ADD COLUMN _mergestat_synced_at timestamp with time zone DEFAULT now() NOT NULL;
 COMMENT ON TABLE public.grype_repo_scans IS 'Table for Grype repo scan results';
 COMMENT ON COLUMN grype_repo_scans.repo_id IS 'foreign key for public.repos.id';
 COMMENT ON COLUMN grype_repo_scans.results IS 'JSON results of Grype repo scanner';
-COMMENT ON COLUMN grype_repo_scans._mergestat_synced_at IS 'timestamp when record was synced into the Mergestat database';
+COMMENT ON COLUMN grype_repo_scans._mergestat_synced_at IS 'timestamp when record was synced into the MergeStat database';
 
 COMMIT;
