ad93ca291c358a52baa9a7ca26f703496fe3d290
Riyaz Ali <riyaz@mergestat.com>
2023-02-09 14:12:37 +0530

    feat: add http basic-auth as a token type

--- migrations/900000000000031_add_git_vendor_and_provider.up.sql
+++ migrations/900000000000031_add_git_vendor_and_provider.up.sql
@@ -7,7 +7,8 @@ VALUES ('git', 'Generic Git');
 INSERT INTO mergestat.service_auth_credential_types (type, description) VALUES
 ('RSA', 'Authentication using RSA Private Keys'),
 ('DSA', 'Authentication using DSA Private Keys'),
-('ECDSA', 'Authentication using ECDSA Private Keys');
+('ECDSA', 'Authentication using ECDSA Private Keys'),
+('BASIC_AUTH', 'Authentication using plain username/password pair');
 
 
 COMMIT;
