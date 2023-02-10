68011520c8ba6c9f4411232c20387d229ef984a3
Ramiro <ramirojesuscastillo95@gmail.com>
2023-02-10 12:33:03 -0500

    feat(migrations):Changing column names and column type for schema

--- migrations/900000000000029_sqlq_support_schema.up.sql
+++ migrations/900000000000029_sqlq_support_schema.up.sql
@@ -1,17 +1,17 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS mergestat.jobs_types (
+CREATE TABLE IF NOT EXISTS mergestat.job_types (
     id uuid PRIMARY KEY DEFAULT public.gen_random_uuid() NOT NULL,
     name text NOT NULL,
     queue text NOT NULL,
     description text,
-    parameters jsonb,
+    default_parameters jsonb,
     priority int,
-    schedule timestamp with time zone,
+    schedule text,
     created_at timestamp with time zone,
     modified_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     FOREIGN KEY (queue) REFERENCES sqlq.queues (name) ON DELETE CASCADE,
+    FOREIGN KEY (schedule) REFERENCES mergestat.job_schedules (schedule)
     CONSTRAINT unique_type UNIQUE(name)
 );
 
@@ -20,22 +20,20 @@ CREATE TABLE IF NOT EXISTS mergestat.jobs (
     queue text NOT NULL,
     type text NOT NULL,
     description text,
-    parameters jsonb,
+    default_parameters jsonb,
     results jsonb,
     priority int,
     created_at timestamp with time zone,
     modified_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     FOREIGN KEY (queue) REFERENCES sqlq.queues (name) ON DELETE CASCADE,
     FOREIGN KEY (type) REFERENCES mergestat.jobs_types (name) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS mergestat.job_schedules (
     job_id uuid NOT NULL,
-    schedule bigint,
+    schedule text,
     created_at timestamp with time zone,
     modified_at timestamp with time zone,
-    deleted_at timestamp with time zone,
     FOREIGN KEY (job_id) REFERENCES mergestat.jobs (id) ON DELETE CASCADE
 );
 
