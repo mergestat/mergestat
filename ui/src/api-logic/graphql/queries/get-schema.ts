24a1a296692dfe7eb251c02b38a80945645f8d96
german-mergestat <german@mergestat.com>
2023-02-15 10:57:26 -0500

    chore(schema): Adjusting query to get schema info sorted

--- ui/src/api-logic/graphql/queries/get-schema.ts
+++ ui/src/api-logic/graphql/queries/get-schema.ts
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client'
 const GET_SCHEMA_INFO = gql`
   query getSchema($search: SqlIdentifier) {
     schemaIntrospections(
-      orderBy: SCHEMA_ASC
+      orderBy: [SCHEMA_ASC, TABLE_NAME_ASC, COLUMN_NAME_ASC]
       filter: {
         or: [
           {schema: {includesInsensitive: $search}},
