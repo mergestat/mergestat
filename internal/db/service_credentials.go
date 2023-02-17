e5954cb4b5275cbbc994c158710fb3acf3a4ed07
Riyaz Ali <riyaz@mergestat.com>
2023-02-17 12:56:21 +0530

    feat: encrypt username before saving

--- internal/db/service_credentials.go
+++ internal/db/service_credentials.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
@@ -9,8 +10,9 @@ import (
 )
 
 // FetchCredential fetches service credential for the given provider and type.
-func (q *Queries) FetchCredential(ctx context.Context, provider uuid.UUID) (username, credential string, err error) {
+func (q *Queries) FetchCredential(ctx context.Context, provider uuid.UUID) (_, _ string, err error) {
 	var secret = os.Getenv("ENCRYPTION_SECRET")
+	var username, credential sql.NullString
 
 	const query = "SELECT username, token FROM mergestat.fetch_service_auth_credential($1, NULL, $2)"
 	var row = q.db.QueryRow(ctx, query, provider, secret)
@@ -18,10 +20,10 @@ func (q *Queries) FetchCredential(ctx context.Context, provider uuid.UUID) (user
 		return "", "", err
 	}
 
-	if len(credential) <= 0 {
+	if !credential.Valid {
 		// default to the `GITHUB_TOKEN` env var if nothing in the DB
-		credential = os.Getenv("GITHUB_TOKEN")
+		credential.String = os.Getenv("GITHUB_TOKEN")
 	}
 
-	return username, credential, nil
+	return username.String, credential.String, nil
 }
