4a1b54cc7ee8e278ac5fe54733a397fdc4fac484
Riyaz Ali <riyaz@mergestat.com>
2023-03-10 11:30:36 +0530

    fix: merge migrations into one and re-generate mock

--- migrations/900000000000047_support_container_based_sync.up.sql
+++ migrations/900000000000047_support_container_based_sync.up.sql
@@ -14,8 +14,11 @@ INSERT INTO mergestat.container_image_types(name, displayName, description) VALU
 -- A registered image from a hosted container registry.
 CREATE TABLE mergestat.container_images (
     id   UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- auto-generated unique identifier for this image
+    name TEXT NOT NULL,                              -- user-provided name of the image
     type TEXT NOT NULL DEFAULT('docker'),            -- type of the container image
     url  TEXT NOT NULL,                              -- full path the image
+    version TEXT NOT NULL DEFAULT ('latest'),        -- version of the image
+    parameters JSONB NOT NULL DEFAULT '{}',          -- any user-defined parameters to pass to the container
 
     -- foreign key constraint for container's type
     CONSTRAINT FK_container_image_type FOREIGN KEY (type) REFERENCES mergestat.container_image_types (name)
@@ -26,7 +29,8 @@ CREATE TABLE mergestat.container_images (
 CREATE TABLE mergestat.container_syncs (
     id           UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- auto-generated unique identifier for this sync
     repo_id      UUID NOT NULL,
-    image_id UUID NOT NULL,
+    image_id     UUID NOT NULL,
+    parameters   JSONB NOT NULL DEFAULT '{}', -- overrides for user-defined parameters
 
     CONSTRAINT FK_sync_repository FOREIGN KEY (repo_id) REFERENCES public.repos (id) ON DELETE CASCADE,
     CONSTRAINT FK_sync_container FOREIGN KEY (image_id) REFERENCES mergestat.container_images (id) ON DELETE CASCADE
@@ -57,5 +61,21 @@ CREATE TABLE mergestat.container_sync_executions (
     CONSTRAINT FK_execution_job FOREIGN KEY (job_id) REFERENCES sqlq.jobs (id)
 );
 
+-- Function: jsonb_recursive_merge()
+-- Recursively merge two json objects together.
+CREATE OR REPLACE FUNCTION jsonb_recursive_merge(a JSONB, b JSONB)
+RETURNS JSONB LANGUAGE sql AS $$
+SELECT
+    jsonb_object_agg(
+        coalesce(ka, kb),
+        CASE
+            WHEN va ISNULL THEN vb
+            WHEN vb ISNULL THEN va
+            WHEN jsonb_typeof(va) <> 'object' OR jsonb_typeof(vb) <> 'object' THEN vb
+            ELSE jsonb_recursive_merge(va, vb) END
+        )
+    FROM jsonb_each(a) e1(ka, va)
+    FULL JOIN jsonb_each(b) e2(kb, vb) ON ka = kb
+$$;
 
 COMMIT;
